1003ebe0127706f0216812275973efe057358d09
Hasibu95 <hyrps8kzsb@privaterelay.appleid.com>
2026-06-05 12:14:49 +0200

    First Upload
    first test after upload the first version

---
+++ script.js
@@ -0,0 +1,66 @@
+let rettungspunkte = [];
+
+fetch('rettungspunkte.json')
+    .then(response => response.json())
+    .then(data => {
+        rettungspunkte = data;
+    });
+
+function rettungspunktSuchen() {
+
+    const eingabe = document
+        .getElementById('suche')
+        .value
+        .trim()
+        .toUpperCase();
+
+    const ergebnisDiv = document.getElementById('ergebnis');
+
+    const punkt = rettungspunkte.find(p =>
+        p.nummer.toUpperCase() === eingabe
+    );
+
+    if (!punkt) {
+        ergebnisDiv.innerHTML = `
+            ❌ Rettungspunkt nicht gefunden.
+        `;
+        return;
+    }
+
+    ergebnisDiv.innerHTML = `
+        <strong>Rettungspunkt:</strong><br>
+        ${punkt.nummer}<br><br>
+
+        <strong>Beschreibung:</strong><br>
+        ${punkt.name}<br><br>
+
+        <strong>Koordinaten:</strong><br>
+        ${punkt.lat}, ${punkt.lon}<br><br>
+
+        <button class="karten-button"
+            onclick="appleKarten(${punkt.lat}, ${punkt.lon})">
+            📍 Apple Karten öffnen
+        </button>
+
+        <button class="karten-button"
+            onclick="googleMaps(${punkt.lat}, ${punkt.lon})">
+            🌍 Google Maps öffnen
+        </button>
+    `;
+}
+
+function appleKarten(lat, lon) {
+        const safeLat = Number(lat);
+        const safeLon = Number(lon);
+    window.open(
+        `https://maps.apple.com/?ll=${safeLat},${safeLon}`,
+        '_blank'
+    );
+}
+
+function googleMaps(lat, lon) {
+    window.open(
+        `https://www.google.com/maps/search/?api=1&query=${lat},${lon}`,
+        '_blank'
+    );
+}
